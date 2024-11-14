a0e25d7263459c885228781e3e72118b34aac579
Ashish <ashishmaan22@gmail.com>
2024-11-14 17:35:37 +0530

    move

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -1,5 +1,6 @@
-//#include "EnhancedInputComponent.h"
 #include "Player/AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
 
 AAuraPlayerController::AAuraPlayerController() {
 	bReplicates = true;
@@ -8,8 +9,8 @@ AAuraPlayerController::AAuraPlayerController() {
 void AAuraPlayerController::BeginPlay() {
 	Super::BeginPlay();
 
-	/*check(AuraContext)
-	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem< UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(AuraContext)
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
 	check(Subsystem)
 	Subsystem->AddMappingContext(AuraContext, 1);
 
@@ -19,6 +20,26 @@ void AAuraPlayerController::BeginPlay() {
 	FInputModeGameAndUI InputModeData;
 	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
 	InputModeData.SetHideCursorDuringCapture(false);
-	SetInputMode(InputModeData);*/
+	SetInputMode(InputModeData);
+}
+
+void AAuraPlayerController::SetupInputComponent() {
+	Super::SetupInputComponent();
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+	check(EnhancedInputComponent);
+
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AAuraPlayerController::Move);
+}
+
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue) {
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
+	const FRotator Rotation = GetControlRotation();
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
 	
+	if(APawn* ControlledPawn = GetPawn<APawn>()) {
+		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+	}
 }
--- Source/Aura/Public/Character/AuraCharacter.h
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -10,4 +10,12 @@
 UCLASS()
 class AURA_API AAuraCharacter : public AAuraCharacterBase {
 	GENERATED_BODY()
+
+public:
+	AAuraCharacter();
+
+protected:
+
+private:
+	
 };
