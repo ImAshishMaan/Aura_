3b3569a178455562a072d333e466688dbf94ef33
Ashish <ashishmaan22@gmail.com>
2024-11-17 17:57:14 +0530

    FFuncPtr to hold the function

--- Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -1,20 +1,21 @@
 #include "UI/WidgetController/AttributeMenuWidgetController.h"
-#include "AuraGameplayTags.h"
 #include "AbilitySystem/AuraAttributeSet.h"
 #include "AbilitySystem/Data/AttributeInfo.h"
 
-void UAttributeMenuWidgetController::BroadcastInitialValues() {
-	Super::BroadcastInitialValues();
-	
-	UAuraAttributeSet* AS = CastChecked<UAuraAttributeSet>(AttributeSet);
-	check(AttributeInfo);
-	FAuraAttributeInfo Info = AttributeInfo->FindAttributeInfoForTag(FAuraGameplayTags::Get().Attributes_Primary_Strength);
-	Info.AttributeValue = AS->GetStrength();
-	AttributeInfoDelegate.Broadcast(Info);
-}
-
 void UAttributeMenuWidgetController::BindCallbacksToDependencies() {
 	Super::BindCallbacksToDependencies();
+}
 
-	
+
+void UAttributeMenuWidgetController::BroadcastInitialValues() {
+	Super::BroadcastInitialValues();
+
+	UAuraAttributeSet* AS = CastChecked<UAuraAttributeSet>(AttributeSet);
+	check(AttributeInfo);
+
+	for(auto& Pair: AS->TagsToAttributes) {
+		FAuraAttributeInfo Info = AttributeInfo->FindAttributeInfoForTag(Pair.Key);
+		Info.AttributeValue = Pair.Value().GetNumericValue(AS);
+		AttributeInfoDelegate.Broadcast(Info);
+	}
 }
