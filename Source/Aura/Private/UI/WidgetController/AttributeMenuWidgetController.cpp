8263dc1e026ce6caf02da090ad622a7587b252e3
Ashish <ashishmaan22@gmail.com>
2024-11-17 11:30:36 +0530

    UAttributeMenuWidgetController

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,13 @@
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BroadcastInitialValues() {
+	Super::BroadcastInitialValues();
+
+	
+}
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies() {
+	Super::BindCallbacksToDependencies();
+
+	
+}
