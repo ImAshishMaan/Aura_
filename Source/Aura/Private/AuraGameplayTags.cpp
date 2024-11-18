996e292c0cb77390210268195bff14976c3dd2fb
Ashish <ashishmaan22@gmail.com>
2024-11-18 11:10:58 +0530

    UAuraInputConfig

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -67,4 +67,33 @@ void FAuraGameplayTags::InitializedNativeGameplayTags() {
 		FName("Attributes.Secondary.MaxMana"),
 		FString("Maximum amount of Mana obtainable")
 	);
+
+	/*
+	 * Input Tags
+	 */
+
+	GameplayTags.InputTag_LMB = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.LMB"),
+		FString("Input tag Left Mouse Button")
+	);
+	GameplayTags.InputTag_RMB = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.RMB"),
+		FString("Input tag Right Mouse Button")
+	);
+	GameplayTags.InputTag_1 = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.1"),
+		FString("Input tag for 1 key")
+	);
+	GameplayTags.InputTag_2 = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.2"),
+		FString("Input tag for 2 key")
+	);
+	GameplayTags.InputTag_3 = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.3"),
+		FString("Input tag for 3 key")
+	);
+	GameplayTags.InputTag_4 = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.4"),
+		FString("Input tag for 4 key")
+	);
 }
--- Source/Aura/Public/AbilitySystem/Abilities/AuraGameplayAbility.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraGameplayAbility.h
@@ -10,4 +10,7 @@
 UCLASS()
 class AURA_API UAuraGameplayAbility : public UGameplayAbility {
 	GENERATED_BODY()
+
+public:
+	
 };
