5717a8297f89a0a3899b0668fada348ffe9c4225
Ashish <ashishmaan22@gmail.com>
2024-11-22 18:39:39 +0530

    data

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,5 @@
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass) {
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
