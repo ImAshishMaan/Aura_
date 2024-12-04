ef439f973d3448a5acc33413db802f877913b032
Ashish <ashishmaan22@gmail.com>
2024-12-04 12:20:13 +0530

    cooldown tags

--- Source/Aura/Public/AbilitySystem/Data/AbilityInfo.h
+++ Source/Aura/Public/AbilitySystem/Data/AbilityInfo.h
@@ -11,6 +11,9 @@ struct FAuraAbilityInfo {
 	
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
 	FGameplayTag AbilityTag = FGameplayTag();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FGameplayTag CooldownTag = FGameplayTag();
 	
 	UPROPERTY(BlueprintReadOnly)
 	FGameplayTag InputTag = FGameplayTag();
