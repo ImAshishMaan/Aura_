d92fcd9de4a1038289ccff7e80c9f68def0079bc
Ashish <ashishmaan22@gmail.com>
2024-12-05 13:27:45 +0530

    level up code

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -1,6 +1,7 @@
 #include "Character/AuraCharacter.h"
 #include "AbilitySystemComponent.h"
 #include "AbilitySystem/AuraAbilitySystemComponent.h"
+#include "AbilitySystem/Data/LevelUpInfo.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "Player/AuraPlayerController.h"
 #include "Player/AuraPlayerState.h"
@@ -11,13 +12,12 @@ AAuraCharacter::AAuraCharacter() {
 	GetCharacterMovement()->RotationRate = FRotator(0.f, 0.f, 400.f);
 	GetCharacterMovement()->bConstrainToPlane = true;
 	GetCharacterMovement()->bSnapToPlaneAtStart = true;
-	
+
 	bUseControllerRotationPitch = false;
-	bUseControllerRotationRoll = false; 
+	bUseControllerRotationRoll = false;
 	bUseControllerRotationYaw = false;
 
 	CharacterClass = ECharacterClass::Elementalist;
-	
 }
 
 void AAuraCharacter::PossessedBy(AController* NewController) {
@@ -26,7 +26,6 @@ void AAuraCharacter::PossessedBy(AController* NewController) {
 	// Init ability actor info for the server
 	InitAbilityActorInfo();
 	AddCharacterAbilities();
-	
 }
 
 void AAuraCharacter::OnRep_PlayerState() {
@@ -48,8 +47,44 @@ void AAuraCharacter::AddToXP_Implementation(int32 InXP) {
 	AuraPlayerState->AddToXP(InXP);
 }
 
-void AAuraCharacter::LevelUp_Implementation() {
-	
+void AAuraCharacter::LevelUp_Implementation() {}
+
+int32 AAuraCharacter::GetXP_Implementation() const {
+	const AAuraPlayerState* AuraPlayerState = GetPlayerState<AAuraPlayerState>();
+	check(AuraPlayerState);
+	return AuraPlayerState->GetXP();
+}
+
+int32 AAuraCharacter::FindLevelForXP_Implementation(int32 InXP) const {
+	const AAuraPlayerState* AuraPlayerState = GetPlayerState<AAuraPlayerState>();
+	check(AuraPlayerState);
+	return AuraPlayerState->LevelUpInfo->FindLevelForXP(InXP);
+}
+
+int32 AAuraCharacter::GetAttributePointsReward_Implementation(int32 Level) const {
+	const AAuraPlayerState* AuraPlayerState = GetPlayerState<AAuraPlayerState>();
+	check(AuraPlayerState);
+	return AuraPlayerState->LevelUpInfo->LevelUpInformation[Level].AttributePointAward;
+}
+
+int32 AAuraCharacter::GetSpellPointsReward_Implementation(int32 Level) const {
+	const AAuraPlayerState* AuraPlayerState = GetPlayerState<AAuraPlayerState>();
+	check(AuraPlayerState);
+	return AuraPlayerState->LevelUpInfo->LevelUpInformation[Level].SpellPointAward;
+}
+
+void AAuraCharacter::AddToPlayerLevel_Implementation(int32 InPlayerLevel) {
+	AAuraPlayerState* AuraPlayerState = GetPlayerState<AAuraPlayerState>();
+	check(AuraPlayerState);
+	AuraPlayerState->AddToLevel(InPlayerLevel);
+}
+
+void AAuraCharacter::AddToAttributePoints_Implementation(int32 InAttributePoints) {
+	//TODO: Add AttributePoints to PlayerState
+}
+
+void AAuraCharacter::AddToSpellPoints_Implementation(int32 InSpellPoints) {
+	//TODO: Add SpellPoints to PlayerState
 }
 
 void AAuraCharacter::InitAbilityActorInfo() {
@@ -69,5 +104,4 @@ void AAuraCharacter::InitAbilityActorInfo() {
 	}
 
 	InitializeDefaultAttributes();
-
 }
