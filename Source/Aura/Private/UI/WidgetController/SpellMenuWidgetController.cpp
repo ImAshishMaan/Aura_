6687ab7c2ccd97826254fa85466e0e70b1625145
Ashish <ashishmaan22@gmail.com>
2025-01-03 12:58:30 +0530

    spell points delegate for update in UI

--- Source/Aura/Private/UI/WidgetController/SpellMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/SpellMenuWidgetController.cpp
@@ -2,13 +2,14 @@
 
 #include "AbilitySystem/AuraAbilitySystemComponent.h"
 #include "AbilitySystem/Data/AbilityInfo.h"
+#include "Player/AuraPlayerState.h"
 
 void USpellMenuWidgetController::BroadcastInitialValues() {
 	BroadcastAbilityInfo();
+	SpellPointsChanged.Broadcast(GetAuraPS()->GetSpellPoints());
 }
 
 void USpellMenuWidgetController::BindCallbacksToDependencies() {
-	
 	GetAuraASC()->AbilityStatusChanged.AddLambda([this](const FGameplayTag& AbilityTag, const FGameplayTag& StatusTag) {
 		if(AbilityInfo) {
 			FAuraAbilityInfo Info = AbilityInfo->FindAbilityInfoForTag(AbilityTag); // get ability info from data asset
@@ -16,5 +17,8 @@ void USpellMenuWidgetController::BindCallbacksToDependencies() {
 			AbilityInfoDelegate.Broadcast(Info);
 		}
 	});
-	
+
+	GetAuraPS()->OnSpellPointsChangedDelegate.AddLambda([this](int32 SpellPoints) {
+		SpellPointsChanged.Broadcast(SpellPoints);
+	});
 }
