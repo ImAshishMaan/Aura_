354a0a5a36e9503e308450e239cec9e53100a866
Ashish <ashishmaan22@gmail.com>
2025-01-05 14:23:35 +0530

    blocking mouse trace if we are already holding mouse buttons

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -337,4 +337,28 @@ void FAuraGameplayTags::InitializedNativeGameplayTags() {
 		FName("InputTag.Passive.2"),
 		FString("Input Tag Passive Ability 2")
 	);
+
+	/*
+	 * Player Tags
+	 */
+
+	GameplayTags.Player_Block_CursorTrace = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Player.Block.CursorTrace"),
+		FString("Block tracing under the cursor")
+	);
+
+	GameplayTags.Player_Block_InputHeld = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Player.Block.InputHeld"),
+		FString("Block Input Held callback for input")
+	);
+
+	GameplayTags.Player_Block_InputPressed = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Player.Block.InputPressed"),
+		FString("Block Input Pressed callback for input")
+	);
+
+	GameplayTags.Player_Block_InputReleased = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Player.Block.InputReleased"),
+		FString("Block Input Released callback for input")
+	);
 }
--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -30,7 +30,8 @@ void AAuraPlayerController::PlayerTick(float DeltaTime) {
 }
 
 void AAuraPlayerController::ShowDamageNumber_Implementation(float DamageAmount, ACharacter* TargetCharacter, bool bBlockedHit, bool bCriticalHit) {
-	if(IsValid(TargetCharacter) && DamageTextComponentClass && IsLocalController()) { // Only show damage text for local player
+	if(IsValid(TargetCharacter) && DamageTextComponentClass && IsLocalController()) {
+		// Only show damage text for local player
 		UDamageTextComponent* DamageText = NewObject<UDamageTextComponent>(TargetCharacter, DamageTextComponentClass);
 		DamageText->RegisterComponent(); // CreateDefaultSubobject do it for us but we need to call it here
 		DamageText->AttachToComponent(TargetCharacter->GetRootComponent(), FAttachmentTransformRules::KeepRelativeTransform); // So it can spawn on top of the character
@@ -70,6 +71,10 @@ void AAuraPlayerController::SetupInputComponent() {
 }
 
 void AAuraPlayerController::Move(const FInputActionValue& InputActionValue) {
+	if(GetASC() && GetASC()->HasMatchingGameplayTag(FAuraGameplayTags::Get().Player_Block_InputPressed)) {
+		return;
+	}
+
 	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
 	const FRotator Rotation = GetControlRotation();
 	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
@@ -83,6 +88,14 @@ void AAuraPlayerController::Move(const FInputActionValue& InputActionValue) {
 }
 
 void AAuraPlayerController::CursorTrace() {
+	if(GetASC() && GetASC()->HasMatchingGameplayTag(FAuraGameplayTags::Get().Player_Block_CursorTrace)) {
+		if(LastActor) LastActor->UnHighlightActor();
+		if(ThisActor) ThisActor->UnHighlightActor();
+		LastActor = nullptr;
+		ThisActor = nullptr;
+		return;
+	}
+	
 	GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
 	if(!CursorHit.bBlockingHit) return;
 	LastActor = ThisActor;
@@ -95,6 +108,10 @@ void AAuraPlayerController::CursorTrace() {
 }
 
 void AAuraPlayerController::AbilityInputTagPressed(FGameplayTag InputTag) {
+	if(GetASC() && GetASC()->HasMatchingGameplayTag(FAuraGameplayTags::Get().Player_Block_InputPressed)) {
+		return;
+	}
+
 	if(InputTag.MatchesTagExact(FAuraGameplayTags::Get().InputTag_LMB)) {
 		bTargeting = ThisActor ? true : false;
 		bAutoRunning = false;
@@ -103,6 +120,10 @@ void AAuraPlayerController::AbilityInputTagPressed(FGameplayTag InputTag) {
 }
 
 void AAuraPlayerController::AbilityInputTagHeld(FGameplayTag InputTag) {
+	if(GetASC() && GetASC()->HasMatchingGameplayTag(FAuraGameplayTags::Get().Player_Block_InputHeld)) {
+		return;
+	}
+
 	if(!InputTag.MatchesTagExact(FAuraGameplayTags::Get().InputTag_LMB)) {
 		if(GetASC()) GetASC()->AbilityInputTagHeld(InputTag);
 		return;
@@ -120,6 +141,10 @@ void AAuraPlayerController::AbilityInputTagHeld(FGameplayTag InputTag) {
 }
 
 void AAuraPlayerController::AbilityInputTagReleased(FGameplayTag InputTag) {
+	if(GetASC() && GetASC()->HasMatchingGameplayTag(FAuraGameplayTags::Get().Player_Block_InputReleased)) {
+		return;
+	}
+
 	if(!InputTag.MatchesTagExact(FAuraGameplayTags::Get().InputTag_LMB)) {
 		if(GetASC()) GetASC()->AbilityInputTagReleased(InputTag);
 		return;
@@ -137,7 +162,9 @@ void AAuraPlayerController::AbilityInputTagReleased(FGameplayTag InputTag) {
 				CachedDestination = NavPath->PathPoints.IsEmpty() ? ControlledPawn->GetActorLocation() : NavPath->PathPoints.Last();
 				bAutoRunning = true;
 			}
-			UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, ClickNiagaraSystem, CachedDestination);
+			if(GetASC() && !GetASC()->HasMatchingGameplayTag(FAuraGameplayTags::Get().Player_Block_InputPressed)) {
+				UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, ClickNiagaraSystem, CachedDestination);
+			}
 		}
 		FollowTime = 0.f;
 		bTargeting = false;
