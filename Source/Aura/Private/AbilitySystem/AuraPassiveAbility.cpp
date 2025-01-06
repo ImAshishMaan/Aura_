63a0640fb9482b55b762d15e37c092a2fc5c2d6e
Ashish <ashishmaan22@gmail.com>
2025-01-06 09:29:38 +0530

    passive spells equip

---
+++ Source/Aura/Private/AbilitySystem/AuraPassiveAbility.cpp
@@ -0,0 +1,19 @@
+#include "AbilitySystem/AuraPassiveAbility.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+
+void UAuraPassiveAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+                                          const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+                                          const FGameplayEventData* TriggerEventData) {
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	if(UAuraAbilitySystemComponent* AuraASC = Cast<UAuraAbilitySystemComponent>(UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(GetAvatarActorFromActorInfo()))) {
+		AuraASC->DeactivatePassiveAbility.AddUObject(this, &UAuraPassiveAbility::ReceiveDeactivate); // Binding the delegate for end ability
+	}
+}
+
+void UAuraPassiveAbility::ReceiveDeactivate(const FGameplayTag& AbilityTag) {
+	if(AbilityTags.HasTagExact(AbilityTag)) {
+		EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, true);
+	}
+}
