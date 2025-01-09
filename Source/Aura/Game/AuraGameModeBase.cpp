3fc44ee71e7ccf9f8c31fda3adbe6148e3b93015
Ashish <ashishmaan22@gmail.com>
2025-01-09 10:25:57 +0530

    player start implementation and range attack bug fix

--- Source/Aura/Game/AuraGameModeBase.cpp
+++ Source/Aura/Game/AuraGameModeBase.cpp
@@ -1,11 +1,12 @@
 #include "AuraGameModeBase.h"
 #include "LoadScreenSaveGame.h"
+#include "GameFramework/PlayerStart.h"
 #include "Kismet/GameplayStatics.h"
 #include "UI/ViewModel/MVVM_LoadSlot.h"
 
 void AAuraGameModeBase::SaveSlotData(UMVVM_LoadSlot* LoadSlot, int32 SlotIndex) {
 	DeleteSlot(LoadSlot->GetLoadSlotName(), SlotIndex);
-	
+
 	USaveGame* SaveGameObject = UGameplayStatics::CreateSaveGameObject(LoadScreenSaveGameClass);
 	ULoadScreenSaveGame* LoadScreenSaveGame = Cast<ULoadScreenSaveGame>(SaveGameObject);
 	LoadScreenSaveGame->PlayerName = LoadSlot->GetPlayerName();
@@ -36,6 +37,24 @@ void AAuraGameModeBase::DeleteSlot(const FString& SlotName, int32 SlotIndex) {
 	if(UGameplayStatics::DoesSaveGameExist(SlotName, SlotIndex)) {
 		UGameplayStatics::DeleteGameInSlot(SlotName, SlotIndex);
 	}
+}
+
+AActor* AAuraGameModeBase::ChoosePlayerStart_Implementation(AController* Player) {
+	TArray<AActor*> Actors;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), APlayerStart::StaticClass(), Actors); // get all player starts
+	if(Actors.Num() > 0) {
+		AActor* SelectedActor = Actors[0];
+		for(AActor* Actor: Actors) {
+			if(APlayerStart* PlayerStart = Cast<APlayerStart>(Actor)) {
+				if(PlayerStart->PlayerStartTag == FName("TheTag")) {
+					SelectedActor = PlayerStart;
+					break;
+				}
+			}
+		}
+		return SelectedActor;
+	}
+	return nullptr;
 }
 
 void AAuraGameModeBase::BeginPlay() {
