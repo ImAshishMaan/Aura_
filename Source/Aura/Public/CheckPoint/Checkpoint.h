6cdf7267fd68b43e4dc76630d5abb9e6c782cc4d
Ashish <ashishmaan22@gmail.com>
2025-01-18 10:05:13 +0530

    highlight non enemy

--- Source/Aura/Public/CheckPoint/Checkpoint.h
+++ Source/Aura/Public/CheckPoint/Checkpoint.h
@@ -36,9 +36,9 @@ protected:
 	
 	void HandleGlowEffects();
 
-private:
-	UPROPERTY(VisibleAnywhere)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	TObjectPtr<UStaticMeshComponent> CheckpointMesh;
+private:
 	
 	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<USphereComponent> Sphere;
